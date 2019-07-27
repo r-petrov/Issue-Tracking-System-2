3a8b7f5d34d74e3ac12353ce92d37fe58d742f60
r-petrov <r_petroff@yahoo.com>
2019-07-27 19:11:20 +0300

    Add IssueLabel joining entity

---
+++ src/Data/IssueTrackingSystem2.Data.Models/IssueLabel.cs
@@ -0,0 +1,13 @@
+﻿namespace IssueTrackingSystem2.Data.Models
+{
+    public class IssueLabel
+    {
+        public string IssueId { get; set; }
+
+        public Issue Issue { get; set; }
+
+        public string LabelId { get; set; }
+
+        public Label Label { get; set; }
+    }
+}
