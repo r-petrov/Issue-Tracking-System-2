2c7d0eb75386f65ca25051ed329ca1c1f483c217
r-petrov <r_petroff@yahoo.com>
2019-07-28 14:24:07 +0300

    Add ProjectLabel Joining entity

---
+++ src/Data/IssueTrackingSystem2.Data.Models/ProjectLabel.cs
@@ -0,0 +1,13 @@
+﻿namespace IssueTrackingSystem2.Data.Models
+{
+    public class ProjectLabel
+    {
+        public string ProjectId { get; set; }
+
+        public Project Project { get; set; }
+
+        public string LabelId { get; set; }
+
+        public Label Label { get; set; }
+    }
+}
