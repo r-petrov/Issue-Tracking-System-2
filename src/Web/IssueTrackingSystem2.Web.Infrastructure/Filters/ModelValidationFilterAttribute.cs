06a213ddb9e33fd1e36a72d0417371938f1eebda
r-petrov <r_petroff@yahoo.com>
2019-08-28 21:54:43 +0300

    Amend Dashboard view

---
+++ src/Web/IssueTrackingSystem2.Web.Infrastructure/Filters/ModelValidationFilterAttribute.cs
@@ -0,0 +1,43 @@
+﻿namespace IssueTrackingSystem2.Web.Infrastructure.Filters
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.AspNetCore.Routing;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ModelValidationFilterAttribute : ActionFilterAttribute
+    {
+        public string RedirectToController { get; set; }
+
+        public string RedirectToAction { get; set; }
+
+        public string RedirectToPage { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new RedirectToRouteResult(ConstructRouteValueDictionary());
+            }
+        }
+
+        private RouteValueDictionary ConstructRouteValueDictionary()
+        {
+            var dict = new RouteValueDictionary();
+
+            if (!string.IsNullOrWhiteSpace(RedirectToPage))
+            {
+                dict.Add("page", RedirectToPage);
+            }
+            else
+            {
+                dict.Add("controller", RedirectToController);
+                dict.Add("action", RedirectToAction);
+            }
+
+            return dict;
+        }
+    }
+}
