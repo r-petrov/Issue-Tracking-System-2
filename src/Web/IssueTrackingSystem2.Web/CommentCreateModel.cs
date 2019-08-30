1b7726b6bdae3aef6b79fd35e6773b3c4d1c3bb5
r-petrov <r_petroff@yahoo.com>
2019-08-30 12:45:03 +0300

    Implement Comment Create Functionality;

---
+++ src/Web/IssueTrackingSystem2.Web/CommentCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTrackingSystem2.Web
+{
+    public class CommentCreateModel
+    {
+        public string Text { get; set; }
+    }
+}
--- src/Web/IssueTrackingSystem2.Web/Controllers/CommentController.cs
+++ src/Web/IssueTrackingSystem2.Web/Controllers/CommentController.cs
@@ -1,17 +1,22 @@
 ﻿namespace IssueTrackingSystem2.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using IssueTrackingSystem2.Common.Infrastructure.Constants;
     using IssueTrackingSystem2.Services.Data.Comment;
     using IssueTrackingSystem2.Services.Data.Issue;
     using IssueTrackingSystem2.Services.Mapping;
     using IssueTrackingSystem2.Services.Models;
+    using IssueTrackingSystem2.Web.Infrastructure.Constants;
+    using IssueTrackingSystem2.Web.Infrastructure.Filters;
+    using IssueTrackingSystem2.Web.InputModels.Comment;
+    using IssueTrackingSystem2.Web.InputModels.Issue;
     using IssueTrackingSystem2.Web.ViewModels.Comment;
     using IssueTrackingSystem2.Web.ViewModels.Issue;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
 
     public class CommentController : BaseController
     {
@@ -27,7 +32,7 @@
         // GET: Comment
         public ActionResult List(string issueId)
         {
-            var commentListServiceModels = this.commentService.All(issueId).ToList();
+            var commentListServiceModels = this.commentService.All(issueId);
             var commentListViewModels = commentListServiceModels.To<CommentListViewModel>().ToList();
             var issueServieModel = this.issueService.ByIdAsync(issueId).GetAwaiter().GetResult();
             var issueViewModel = issueServieModel.To<CommentListIssueViewModel>();
@@ -42,48 +47,76 @@
         }
 
         // GET: Comment/Create
-        public ActionResult Create()
+        [HttpGet]
+        [IssueAssigneeFilterAttribute]
+        public async Task<ActionResult> Create(string issueId, string leaderId, string assigneeId)
         {
-            return View();
+            try
+            {
+                var issueConciseInputModel = await this.SetIssueConciseInputModel(issueId);
+                var commentCreateInputModel = new CommentCreateInputModel()
+                {
+                    Issue = issueConciseInputModel,
+                };
+
+                return this.View(commentCreateInputModel);
+            }
+            catch (Exception ex)
+            {
+                this.ViewData[ValuesConstants.InvalidArgument] = ex.Message;
+
+                return this.RedirectToAction(
+                    actionName: nameof(this.List),
+                    routeValues: new { issueId = issueId });
+            }
         }
 
         // POST: Comment/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [IssueAssigneeFilterAttribute]
+        public async Task<ActionResult> Create(
+            CommentCreateInputModel commentCreateInputModel,
+            string issueId,
+            string leaderId,
+            string assigneeId)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (commentCreateInputModel == null)
+                {
+                    this.ViewData[ValuesConstants.InvalidArgument] = string.Format(
+                        format: MessagesConstants.NullOrEmptyArgument,
+                        arg0: nameof(commentCreateInputModel));
 
-                return this.RedirectToAction(nameof(this.List));
+                    return this.View();
+                }
+
+                if (!this.ModelState.IsValid)
+                {
+                    var issueConciseInputModel = await this.SetIssueConciseInputModel(issueId);
+                    commentCreateInputModel.Issue = issueConciseInputModel;
+
+                    return this.View(commentCreateInputModel);
+                }
+
+                var commentServiceModel = commentCreateInputModel.To<CommentServiceModel>();
+                commentServiceModel.IssueId = issueId;
+                commentServiceModel.CreatorId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                var commentServiceModelResult = await this.commentService.CreateAsync(commentServiceModel);
+                var commentCreateInputModelResult = commentServiceModelResult.To<CommentCreateInputModel>();
+
+                return this.RedirectToAction(
+                    actionName: nameof(this.List), 
+                    routeValues: new { issueId = issueId });
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
-            }
-        }
+                this.ViewData[ValuesConstants.InvalidArgument] = ex.Message;
+                var issueConciseInputModel = await this.SetIssueConciseInputModel(issueId);
+                commentCreateInputModel.Issue = issueConciseInputModel;
 
-        // GET: Comment/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Comment/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(this.List));
-            }
-            catch
-            {
-                return View();
+                return this.View(commentCreateInputModel);
             }
         }
 
@@ -108,6 +141,22 @@
             {
                 return View();
             }
+        }
+
+        private async Task<IssueConciseInputModel> SetIssueConciseInputModel(string issueId)
+        {
+            var issueServiceModel = await this.issueService.ByIdAsync(issueId);
+            if (issueServiceModel == null)
+            {
+                throw new Exception(string.Format(
+                        format: MessagesConstants.NullItem,
+                        arg0: GlobalConstants.Issue,
+                        arg1: nameof(issueId),
+                        arg2: issueId));
+            }
+
+            var issueConciseInputModel = issueServiceModel.To<IssueConciseInputModel>();
+            return issueConciseInputModel;
         }
     }
 }
